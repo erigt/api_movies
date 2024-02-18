900815e805ddc740b0410fa28a908ab2be86f70d
Erika <146768635+erigt@users.noreply.github.com>
2024-02-18 20:26:56 +0100

    errores

--- src/main/java/dev/erika/api_movies/controllers/MovieController.java
+++ src/main/java/dev/erika/api_movies/controllers/MovieController.java
@@ -2,6 +2,7 @@ package dev.erika.api_movies.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
---
+++ src/main/java/dev/erika/api_movies/service/Message.java
@@ -0,0 +1,5 @@
+package dev.erika.api_movies.service;
+
+public class Message {
+
+}
